666ece232c72b3d385365a2adcf5ffbaddc92bb3
Victor Elgersma <elgersmav@gmail.com>
2025-12-02 00:24:54 +0100

    yo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ..
-rsync -a photo_portfolio elgersmav@hetzner:/var/www/victorelgersma.ddns.net/html/dev
+rsync -a photo_portfolio elgersmav@hetzner:/var/www/victorelgersma.ddns.net/html/dev/ahmed_photo_portfolio
